d2c90152f3e52c7eff75abbed49d1e69112492da
Leonard Flachs <leonard.flachs@leanovate.de>
2023-02-03 10:21:31 +0100

    Remove solution

--- cypress/support/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    /**
-     * Custom command to select DOM element by data-cy attribute.
-     * @example cy.dataCy('greeting')
-     */
-    login(username: string, password: string): void;
-  }
-}
